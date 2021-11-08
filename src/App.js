4321e57a02b61e5fe6a76cfe3db50e0281c6bf39
bruna-devbio <bruna.ppp8@gmail.com>
2021-11-08 15:07:08 -0300

    rotas iniciais

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Login from './pages/Login/login';
+
 
 function App() {
   return (
-    <div className="App">
-      ...
+    <div>
+      <main>
+        <Routes>
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+      </main>
     </div>
   );
 }
