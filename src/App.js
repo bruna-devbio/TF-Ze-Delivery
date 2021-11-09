e939d623191c42a8a58eb0f5d6322a0eeeb476b4
Michele Martins <michele.valerio@outlook.com.br>
2021-11-09 16:06:13 -0300

    mergeando novas features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Login from './pages/Login/Login'
 import Home from './pages/Home/Home'
-import Register from './pages/Register/Register'
+
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
@@ -9,8 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="/Register" element={<Register />}/>
-      <Route path="/Login" element={<Login />}/>
+    
     </Routes>
   );
 }
--- src/pages/Register/Register.js
+++
@@ -1,8 +0,0 @@
-
-const Register = () => {
-  return (
-    <h1>Cadastro</h1>
-  )
-}
-
-export default Register
