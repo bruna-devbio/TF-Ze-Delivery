15584d71f6a90318cd9f85fbf20cc6aa0db64444
Sabrina <sabrinaaraujo.ds@gmail.com>
2021-11-09 18:11:45 -0300

    cpf mask, card mask

--- src/components/InputMask/InputMask.js
+++ src/components/InputMask/InputMask.js
@@ -2,9 +2,11 @@ import React from 'react';
 import InputMask from 'react-input-mask';
 
 
-const InputMasked = ({ value, onChange, name, mask }) => {
+const InputMasked = ({type, placeholder, value, onChange, name, mask }) => {
 
-  const onlyNumbers = (str) => str.replace(/[^0-9]/g, '');
+  /*const onlyNumbers = (str) => str.replace(/[^0-9]/g, '');
+  
+
   function handleChange(e) {
     onChange({
       ...e,
@@ -14,15 +16,17 @@ const InputMasked = ({ value, onChange, name, mask }) => {
         value: onlyNumbers(e.target.value)
       }
     });
-  }
+  }*/
 
   // tela cadastro
   return (
     <InputMask
-      name="cpf"
+      name={name}
+      placeholder={placeholder}
+      type={type}
       mask={mask}
       value={value}
-      onChange={handleChange}
+      onChange={onChange}
     />
   );
   // 
