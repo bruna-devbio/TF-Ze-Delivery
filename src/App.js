24ee5e005e7a288c2819cc7af77aea5af985a62a
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-11-08 21:49:43 -0300

    renderização do input c/ CPF

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import MaskRegister from './components/InputMask/form.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      ...
+      ... <MaskRegister />
     </div>
   );
 }
