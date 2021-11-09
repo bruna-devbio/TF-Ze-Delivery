1ae7f543111673338494fd876d4cb7872ba042ea
KamilaMoura1 <kamila.demoura93@gmail.com>
2021-11-09 18:30:55 +0000

    atualização voucher

--- src/components/MainVoucher/index.js
+++ src/components/MainVoucher/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Input from "../Input";
 import imgVoucher from "../../assets/img/imgVoucher.png";
@@ -6,6 +6,17 @@ import "./index.css";
 import Button from "../Button";
 
 const MainVoucher = () => {
+
+  const [values, setValues] = useState({
+		voucher: '',
+	});
+
+	const onChangeValues = (event) => {
+		setValues({
+			...values,
+			[event.target.voucher]: event.target.value
+		})
+	};
 
   const navigateVoucher = useNavigate()
   navigateVoucher("/Voucher");
@@ -19,9 +30,9 @@ const MainVoucher = () => {
             type="text"
             placeholder="Valor do Voucher"
             className="voucher-input"
-            name="valor"
-            //onChange={onChangeValues}
-            //value={values.email}
+            name="voucher"
+            onChange={onChangeValues}
+            value={values.voucher}
           ></Input>
         </div>
         <div className="div-img">
@@ -37,7 +48,7 @@ const MainVoucher = () => {
         </p>
 
         <h3 className="text title">Pedido mínimo de R$150,00</h3>
-        
+
         <div className="div-button-voucher">
           <Button
             type="submit"
